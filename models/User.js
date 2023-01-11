56be744a22f34f8f4788752ffc9b6ea364b33eaf
CinYP <111353883+CinYP@users.noreply.github.com>
2023-01-10 19:16:14 -0500

    first commit

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      max_length: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      max_length: 50,
+    },
+    thoughts: [
+        {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought',
+    },
+    ],
+    friends: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: 'User',  
+        }
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function (){
+    return this.friends.length; 
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
