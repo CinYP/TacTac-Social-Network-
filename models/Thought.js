b8155979061fc46304c1cf3c1f5ccff0d7114939
CinYP <111353883+CinYP@users.noreply.github.com>
2023-01-10 19:26:16 -0500

    thought model

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, Types } = require('mongoose');
+const reactionSchema = require('./Reaction'); 
+const dateFormat = require('../utils/dateFormat');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
+    },
+    username: {
+        type: String,
+        required: true
+      },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+thoughtSchema.virtual('reactionCount').get(function(){
+    return this.reaction.length
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+
+module.exports = Thought;
