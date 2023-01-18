cb900b8e2a118f8e5ce56f08d235bd40f5722491
CinYP <111353883+CinYP@users.noreply.github.com>
2023-01-18 17:33:04 -0500

    delete user fixed

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -17,9 +17,11 @@ module.exports = {
   },
   // Get a single user
   getSingleUser(req, res) {
-    user.findOne({ _id: req.params.userId })
+    User.findOne({ _id: req.params.userId })
       .select('-__v')
-      .then(async (user) =>
+      .populate('friends')
+      .populate('thoughts')
+      .then( (user) =>
         !user
           ? res.status(404).json({ message: 'No user with that ID' })
           : res.json({
@@ -31,31 +33,25 @@ module.exports = {
         return res.status(500).json(err);
       });
   },
+
   // create a new user
   createUser(req, res) {
-    user.create(req.body)
+    User.create(req.body)
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
+
   // Delete a user and remove them from the course
   deleteUser(req, res) {
-    user.findOneAndRemove({ _id: req.params.userId })
-      .then((user) =>
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) => {
         !user
           ? res.status(404).json({ message: 'No such user exists' })
-          : Course.findOneAndUpdate(
-              { users: req.params.userId },
-              { $pull: { users: req.params.userId } },
-              { new: true }
-            )
-      )
-      .then((course) =>
-        !course
-          ? res.status(404).json({
-              message: 'user deleted, but no courses found',
-            })
-          : res.json({ message: 'user successfully deleted' })
-      )
+          : res.json(user); 
+      })
+      .then(() =>{
+          res.json({ message: 'user successfully deleted' })
+          })
       .catch((err) => {
         console.log(err);
         res.status(500).json(err);
